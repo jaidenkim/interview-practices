3449705b55d30a8895158e7074a281b45f161b5a
jaiden <125882208+jaidenkim@users.noreply.github.com>
2025-03-04 22:46:36 +0900

    feat: add 1512

---
+++ hash_tables/leetcode_1512_number_of_good_pairs.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        number2idx = {}
+        cnt = 0
+        for i, n in enumerate(nums):
+            if n in number2idx:
+                cnt += len(number2idx[n])
+                number2idx[n].append(i)
+            else:
+                number2idx[n] = [i]
+        return cnt
